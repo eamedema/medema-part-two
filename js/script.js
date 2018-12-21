7a067bbd3631e68b49c1f3a68e9503629031a8a9
eamedema <eamedema@gmail.com>
2018-12-20 21:23:11 -0500

    finished deliverable two

--- js/script.js
+++ js/script.js
@@ -1,23 +1,62 @@
 // JavaScript Document
 
-var select = document.querySelector('select');
-var para = document.querySelector('p');
-var temperature = 29;
-select.onchange = eventType;
+function run() {
 
-	function eventType() {
-  		var choice = select.value;
-		 switch (choice) {
-			case 'casual':
-			  para.textContent = 'something comfy';
-			  break;
-			case 'semi-formal':
-			  para.textContent = 'a polo';
-			  break;
-			case 'formal':
-			  para.textContent = 'a suit';
-			  break;
-			default:
-			  para.textContent = '';
-		  }
+	document.getElementById("submit").addEventListener("click", function() {
+
+		var tempFahr = document.getElementById("airTemp");
+
+		var eventType = document.getElementById("affairType");
+
+		whatToWear(tempFahr.value, eventType.value)
+	});
+
+		function whatToWear(airTemp, affair){
+			var casual = "somthing comfy"
+			var semiFormal = "a polo"
+			var formal = "a suit"
+
+			var coat = "a coat"
+			var jacket = "a jacket"
+			var noJacket = "no jacket"
+
+			var a = "Since it is "
+			var b = " degrees and you are going to a "
+			var c = " event, you should wear "
+			var d = " & "
+
+			var result;
+
+				if (affair === "casual" && 54 <= airTemp || airTemp <= 70) {
+					result = a + airTemp + b + affair + c + casual + d + jacket
+
+				} else if (affair === "casual" && airTemp < 54) {
+					result = a + airTemp + b + affair + c + casual + d + coat
+
+				} else if (affair === "casual" && airTemp > 70) {
+					result = a + airTemp + b + affair + c + casual + d + noJacket
+
+				} else if (affair === "semi-formal" && 54 <= airTemp || airTemp <= 70) {
+					result = a + airTemp + b + affair + c + semiFormal + d + jacket
+
+				} else if (affair === "semi-formal" && airTemp < 54) {
+					result = a + airTemp + b + affair + c + semiFormal + d + jacket
+
+				} else if (affair === "semi-formal" && airTemp > 70) {
+					result = a + airTemp + b + affair + c + semiFormal + d + jacket
+
+				} else if (affair === "formal" && 54 <= airTemp || airTemp <= 70) {
+					result = a + airTemp + b + affair + c + formal + d + jacket
+
+				} else if (affair === "formal" && airTemp < 54) {
+					result = a + airTemp + b + affair + c + formal + d + coat
+
+				} else if (affair === "formal" && airTemp > 70) {
+					result = a + airTemp + b + affair + c + formal + d + noJacket
+
+				}
+				console.log(result);
 		}
+}
+
+window.onload = run;
